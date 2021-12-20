f8c1dfc37a2733a79b7fdcf4533461a46f132d64
Pavel Goytan <3160398@gmail.com>
2021-12-21 01:14:19 +0300

    4

--- TaskB/Chapter1/TaskB1.java
+++ TaskB/Chapter1/TaskB1.java
@@ -1,5 +1,7 @@
 package TaskB.Chapter1;
 
+import java.util.Scanner;
+
 /**
  * Вариант B
  * Ввести с консоли n целых чисел. На консоль вывести:
@@ -18,4 +20,47 @@ package TaskB.Chapter1;
  * 11. Элементы, которые равны полусумме соседних элементов.
  */
 public class TaskB1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] array = line.split(" ");
+        int[] arrayInt = new int[array.length];
+        StringBuilder evenInt = new StringBuilder();
+        StringBuilder oddInt = new StringBuilder();
+        StringBuilder divideByThree = new StringBuilder();
+        StringBuilder divideByFive = new StringBuilder();
+        int max = 0;
+        int min;
+        for (int j = 0; j < array.length; j++) {
+            arrayInt[j] = Integer.parseInt(array[j]);
+        }
+        min = arrayInt[0];
+        for (int j : arrayInt) {
+
+            max = Math.max(max, j);
+            min = Math.min(min, j);
+            if (j % 2 == 0) {
+                evenInt.append(j).append(" ");
+            } else if (j % 2 > 0) {
+                oddInt.append(j).append(" ");
+            }
+        }
+        for (int j : arrayInt) {
+            if (j % 3 == 0) {
+                divideByThree.append(j).append(" ");
+            } else if (j % 5 == 0 && j % 7 == 0) {
+                divideByFive.append(j).append(" ");
+            }
+        }
+        System.out.println(evenInt);
+        System.out.println(oddInt);
+        System.out.println(min);
+        System.out.println(max);
+        System.out.println(divideByThree);
+        System.out.println(divideByFive);
+
+    }
+
+
 }
