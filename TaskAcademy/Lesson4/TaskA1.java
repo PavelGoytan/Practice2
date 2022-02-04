32df90427f31b12e36b8fd312863a691309db4a0
Pavel Goytan <3160398@gmail.com>
2022-02-05 01:35:37 +0300

    23

--- TaskAcademy/Lesson4/TaskA1.java
+++ TaskAcademy/Lesson4/TaskA1.java
@@ -1,5 +1,7 @@
 package TaskAcademy.Lesson4;
 
+import java.util.Arrays;
+
 /**
  * напишите в нем метод
  * static void printMulTable() для вывода на консоль таблицы умножения в следующем виде (с
@@ -10,27 +12,40 @@ public class TaskA1 {
         printMulTable();
         String line = "12.3 11.4 15.6 16.7 17.4 78.26";
         double[] array = InOut.getArray(line);
-        printArray(array,5);
-        Helper.sortDouble(array,true);
-        printArray(array,4);
+//        printArray(array,5);
+//        Helper.sortDouble(array,true);
+//        printArray(array,4);
+        double[][] doubles = sortMerge(array);
+        System.out.println(doubles[5][0]);
+        System.out.println(Arrays.deepToString(doubles));
 
     }
 
-    private static void printArray(double[] array,int cols) {
-        int col=0;
-        for (int i = 0; i< array.length; i++) {
-            System.out.printf("%3s%d%3s%-3.4f%3s","V[ ",i+1," ]=", array[i],"");
+    private static void printArray(double[] array, int cols) {
+        int col = 0;
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%3s%d%3s%-3.4f%3s", "V[ ", i + 1, " ]=", array[i], "");
             col++;
-            if(col%cols==0||i== array.length-1){
+            if (col % cols == 0 || i == array.length - 1) {
                 System.out.println();
             }
         }
     }
 
-    static void  printMulTable(){
-        for (int i = 2; i <=9; i++) {
-            for (int j = 2; j <=9; j++) {
-                System.out.printf("%1d%1s%1d%1s%2d%2s",i,"*",j,"=",i*j,"");
+    private static double[][] sortMerge(double[] array) {
+        double[][] merge = new double[array.length][1];
+        for (int i = 0; i < merge.length; i++) {
+            for (int j = 0; j < merge[i].length; j++) {
+                merge[i][j] = array[i];
+            }
+        }
+        return merge;
+    }
+
+    static void printMulTable() {
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 2; j <= 9; j++) {
+                System.out.printf("%1d%1s%1d%1s%2d%2s", i, "*", j, "=", i * j, "");
             }
             System.out.println();
         }
---
+++ TaskAcademy/Lesson6/TaskA1.java
@@ -0,0 +1,18 @@
+package TaskAcademy.Lesson6;
+
+import java.util.stream.IntStream;
+
+
+/**
+ *TaskA1. В каждом слове текста 4-ю и 7-ю буквы заменить символом #. Для слов короче 4
+ * символов корректировку не выполнять. Для слов короче 7 символов заменять только 4-ю букву.
+ */
+public class TaskA1 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder(Poem.text);
+        IntStream intStream = stringBuilder.chars();
+
+
+
+    }
+}
